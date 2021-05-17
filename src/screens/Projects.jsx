9d896c77f80ee23b73361393e52b75ef501b8057
Amanda <amandalphail@gmail.com>
2021-05-17 16:57:17 -0400

    link my work

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -4,12 +4,23 @@ export default function Projects() {
   return (
     <div id="projects">
       <h1>Projects</h1>
+
       <h2>Skate Bored</h2>
       <p>Description and skills used</p>
+      <a href="http://skate-bored.surge.sh/">Visit Site</a>
+      <a href="https://github.com/amandaphail/Skate-Bored">Code</a>
+      
+      
       <h2>Experience a Memory</h2>
       <p>Description and skills used</p>
+      <a href="https://experience-a-memory.netlify.app/">Visit Site</a>
+      <a href ="https://github.com/amandaphail/VisualizeAMemory">Code</a>
+      
+
       <h2>The Gallery</h2>
       <p>Description and skills used</p>
+      <a href="https://thegalleryforyou.netlify.app/">Visit Site</a>
+      <a href ="https://github.com/amandaphail/TheGallery">Code</a>
     
     </div>
   )
