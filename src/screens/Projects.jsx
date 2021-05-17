c138553543ec133e75739e764840389eb009ec75
Amanda <amandalphail@gmail.com>
2021-05-17 17:10:09 -0400

    stylings

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -2,26 +2,34 @@ import "./CSS/projects.css"
 
 export default function Projects() {
   return (
-    <div id="projects">
+    <div id="projectspage">
       <h1>Projects</h1>
 
+      <div id="projects">
+
+      <div>
       <h2>Skate Bored</h2>
       <p>Description and skills used</p>
       <a href="http://skate-bored.surge.sh/">Visit Site</a>
       <a href="https://github.com/amandaphail/Skate-Bored">Code</a>
+      </div>
       
-      
+      <div>
       <h2>Experience a Memory</h2>
       <p>Description and skills used</p>
       <a href="https://experience-a-memory.netlify.app/">Visit Site</a>
       <a href ="https://github.com/amandaphail/VisualizeAMemory">Code</a>
+      </div>
       
 
+      <div>
       <h2>The Gallery</h2>
       <p>Description and skills used</p>
       <a href="https://thegalleryforyou.netlify.app/">Visit Site</a>
       <a href ="https://github.com/amandaphail/TheGallery">Code</a>
+      </div>
     
+      </div>
     </div>
   )
 }
