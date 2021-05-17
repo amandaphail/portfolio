8c8fd86e6a821ccd1e2537acdb35fdb51ea34181
Amanda <amandalphail@gmail.com>
2021-05-17 13:08:31 -0400

    basics

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Projects() {
   return (
     <div>
-      Projects
+      <h1>Projects</h1>
+      <h2>Skate Bored</h2>
+      <p>Description and skills used</p>
+      <h2>Experience a Memory</h2>
+      <p>Description and skills used</p>
+      <h2>The Gallery</h2>
+      <p>Description and skills used</p>
+    
     </div>
   )
 }
