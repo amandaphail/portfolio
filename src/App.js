01f2675ff69a6c40411befc321dc3cbc9a2750c0
Amanda <amandalphail@gmail.com>
2021-05-17 12:51:35 -0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom"
+import Title from "./screens/Title.jsx"
+import AboutMe from "./screens/AboutMe.jsx"
+import Projects from "./screens/Projects.jsx"
+import Contact from "./components/Contact.jsx"
 
 function App() {
   return (
     <div className="App">
       App
+      <Route>
+        <Title />
+        <AboutMe />
+        <Projects />
+        <Contact />
+      </Route>
     </div>
   );
 }
