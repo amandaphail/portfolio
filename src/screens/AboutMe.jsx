331f5f8b8296c3f64cab8a7ab6917efa99be9d42
Amanda <amandalphail@gmail.com>
2021-05-18 13:11:44 -0400

    about me reminders

--- src/screens/AboutMe.jsx
+++ src/screens/AboutMe.jsx
@@ -6,6 +6,8 @@ export default function AboutMe() {
       <h1>About Me</h1>
       <p id="content">Resilient software engineer with a client-facing background, always excited to explore in the world, and in technology. While I value both my independence and freedom, I am also a supportive team player, finding success through creativity, confidence, understanding and open mindedness.</p>
       {/* link to github */}
+      <p>Checkout my GitHub and LinkedIn!</p>
+      <p>You can download my Resume here!</p>
     </div>
   )
 }
