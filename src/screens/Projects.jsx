beba063ea6899d468acaa01e431d4e5f65f21de3
Amanda <amandalphail@gmail.com>
2021-05-19 12:11:45 -0400

    projects stylings

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -10,29 +10,36 @@ export default function Projects() {
 
       <div id="projects">
 
-      <div>
-          <h2>Skate Bored</h2>
+      <div id="proj">
           <img id="projimg" alt="skate bored website" src={SkateBored} />
+          <div>
+          <h2>Skate Bored</h2>
           <p>Description and skills used</p>
           <a href="http://skate-bored.surge.sh/">Visit Site</a>
           <a href="https://github.com/amandaphail/Skate-Bored">Code</a>
+          </div>
       </div>
       
-      <div>
-          <h2>Experience a Memory</h2>
+        <div id="proj">
           <img id="projimg" alt="Experience a Memory website" src={Memory} />
+          <div id="center">
+          <h2 id="long">Experience a Memory</h2>
           <p>Description and skills used</p>
           <a href="https://experience-a-memory.netlify.app/">Visit Site</a>
           <a href ="https://github.com/amandaphail/VisualizeAMemory">Code</a>
+          </div>
+
       </div>
       
 
-      <div>
-          <h2>The Gallery</h2>
+        <div id="proj">   
           <img id="projimg" alt="The Gallery website" src={Gallery} />
+          <div>
+          <h2>The Gallery</h2>
           <p>Description and skills used</p>
           <a href="https://thegalleryforyou.netlify.app/">Visit Site</a>
           <a href ="https://github.com/amandaphail/TheGallery">Code</a>
+          </div>
       </div>
     
       </div>
