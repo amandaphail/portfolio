0d88222c1286ae63855843f45ba9f54a3ad5265f
Amanda <amandalphail@gmail.com>
2021-05-19 16:18:36 -0400

    video background working with text

--- src/screens/Title.jsx
+++ src/screens/Title.jsx
@@ -4,12 +4,15 @@ import background from "../videos/background.mp4"
 export default function Title() {
   return (
     <div id="title">
-    <video autoPlay muted loop id="myVideo">
-  <source src={background} type="video/mp4"/>
-</video>
-      {/* <h1>Amanda Phail</h1>
-      <h2>Software Engineer,</h2>
-      <h3>FullStack Developer</h3> */}
+      <video autoPlay muted loop id="myVideo">
+        <source src={background} type="video/mp4"/>
+      </video>
+      
+      <div id="mytitle">
+        <h1>Amanda Phail</h1>
+        <h2>Software Engineer,</h2>
+        <h3>FullStack Developer</h3>
+      </div>
     </div>
   )
 }
