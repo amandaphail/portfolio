5c07158e9ac38f5444e2eebf0048737f9a98aa1b
Amanda <amandalphail@gmail.com>
2021-05-19 15:50:44 -0400

    title page video background working

--- src/screens/Title.jsx
+++ src/screens/Title.jsx
@@ -1,11 +1,15 @@
 import "./CSS/title.css"
+import background from "../videos/background.mp4"
 
 export default function Title() {
   return (
-    <div id ="title">
-      <h1>Amanda Phail</h1>
+    <div id="title">
+    <video autoPlay muted loop id="myVideo">
+  <source src={background} type="video/mp4"/>
+</video>
+      {/* <h1>Amanda Phail</h1>
       <h2>Software Engineer,</h2>
-      <h3>FullStack Developer</h3>
+      <h3>FullStack Developer</h3> */}
     </div>
   )
 }
