f5e3e396ed960359bce4fad5b95d145a047f6211
Amanda <amandalphail@gmail.com>
2021-05-18 21:03:24 -0400

    me image

--- src/screens/AboutMe.jsx
+++ src/screens/AboutMe.jsx
@@ -1,13 +1,21 @@
 import "./CSS/aboutme.css"
+import me from "../images/me.jpeg"
 
 export default function AboutMe() {
   return (
     <div id ="aboutme">
       <h1>About Me</h1>
-      <p id="content">Resilient software engineer with a client-facing background, always excited to explore in the world, and in technology. While I value both my independence and freedom, I am also a supportive team player, finding success through creativity, confidence, understanding and open mindedness.</p>
-      {/* link to github */}
-      <p>Checkout my GitHub and LinkedIn!</p>
-      <p>You can download my Resume here!</p>
+      <div id="body">
+        <img id="me" src={me}></img>
+
+        <div id ="content">
+          <p>Resilient software engineer with a client-facing background, always excited to explore in the world, and in technology. While I value both my independence and freedom, I am also a supportive team player, finding success through creativity, confidence, understanding and open mindedness.</p>
+
+          <p>Checkout my <a href="https://github.com/amandaphail">Github</a> and <a href="https://github.com/amandaphail">LinkedIn</a>!</p>
+
+          <p>You can download my Resume here!</p>
+        </div>
+      </div>
     </div>
   )
 }
