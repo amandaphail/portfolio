c4908b301b36121f1fc2d4da1e29dcbf62054f88
Amanda <amandalphail@gmail.com>
2021-05-19 14:02:34 -0400

    project descriptions

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
           <img id="projimg" alt="skate bored website" src={SkateBored} />
           <div>
           <h2>Skate Bored</h2>
-          <p>Description and skills used</p>
+          <p>Using NYC Data API, this application catalogs the Skate Parks in NYC and allows the user to find skate parks by location and its features.</p>
           <a href="http://skate-bored.surge.sh/" target="_blank" rel="noopener noreferrer">Visit Site</a>
           <a href="https://github.com/amandaphail/Skate-Bored" target="_blank" rel="noopener noreferrer">Code</a>
           </div>
@@ -24,7 +24,7 @@ export default function Projects() {
           <img id="projimg" alt="Experience a Memory website" src={Memory} />
           <div id="center">
           <h2 id="long">Experience a Memory</h2>
-          <p>Description and skills used</p>
+          <p>React application using Google Maps "Embed a map" feature creates a communal memoir where users can post their memories using Airtable, an external API.</p>
           <a href="https://experience-a-memory.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
           <a href ="https://github.com/amandaphail/VisualizeAMemory" target="_blank" rel="noopener noreferrer">Code</a>
           </div>
@@ -36,7 +36,7 @@ export default function Projects() {
           <img id="projimg" alt="The Gallery website" src={Gallery} />
           <div>
           <h2>The Gallery</h2>
-          <p>Description and skills used</p>
+          <p>Using React and Ruby on Rails as tools, users create a blueprint for a gallery wall using their own images, giving them a visual guidance to install one in their homes.</p>
           <a href="https://thegalleryforyou.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
           <a href ="https://github.com/amandaphail/TheGallery"target="_blank" rel="noopener noreferrer">Code</a>
           </div>
