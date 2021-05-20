ce0c9f228fa3840f9d98811accde73b5d3aa5117
Amanda <amandalphail@gmail.com>
2021-05-20 13:57:59 -0400

    content styling

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -12,21 +12,26 @@ export default function Projects() {
 
       <div id="proj">
           <img id="projimg" alt="skate bored website" src={SkateBored} />
-          <div>
+          <div id="projcontent">
           <h2>Skate Bored</h2>
-          <p>Using NYC Data API, this application catalogs the Skate Parks in NYC and allows the user to find skate parks by location and its features.</p>
-          <a href="http://skate-bored.surge.sh/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-          <a href="https://github.com/amandaphail/Skate-Bored" target="_blank" rel="noopener noreferrer">Code</a>
+            <p id="left">Using NYC Data API, this application catalogs the Skate Parks in NYC and allows the user to find skate parks by location and its features.</p>
+
+            <div id="links">
+          <a id ="site" href="http://skate-bored.surge.sh/" target="_blank" rel="noopener noreferrer">Visit Site</a>
+          <a id="code" href="https://github.com/amandaphail/Skate-Bored" target="_blank" rel="noopener noreferrer">Code</a>
+            </div>
           </div>
       </div>
       
         <div id="proj">
           <img id="projimg" alt="Experience a Memory website" src={Memory} />
-          <div id="center">
+          <div id="projcontent">
           <h2 id="long">Experience a Memory</h2>
-          <p>React application using Google Maps "Embed a map" feature creates a communal memoir where users can post their memories using Airtable, an external API.</p>
-          <a href="https://experience-a-memory.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-          <a href ="https://github.com/amandaphail/VisualizeAMemory" target="_blank" rel="noopener noreferrer">Code</a>
+            <p id="left">React application using Google Maps "Embed a map" feature creates a communal memoir where users can post their memories using Airtable, an external API.</p>
+            <div id ="links">
+          <a id ="site" href="https://experience-a-memory.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
+          <a id="code" href ="https://github.com/amandaphail/VisualizeAMemory" target="_blank" rel="noopener noreferrer">Code</a>
+            </div>
           </div>
 
       </div>
@@ -34,11 +39,14 @@ export default function Projects() {
 
         <div id="proj">   
           <img id="projimg" alt="The Gallery website" src={Gallery} />
-          <div>
+          <div id="projcontent">
           <h2>The Gallery</h2>
-          <p>Using React and Ruby on Rails as tools, users create a blueprint for a gallery wall using their own images, giving them a visual guidance to install one in their homes.</p>
-          <a href="https://thegalleryforyou.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-          <a href ="https://github.com/amandaphail/TheGallery"target="_blank" rel="noopener noreferrer">Code</a>
+            <p id="left">Using React and Ruby on Rails as tools, users create a blueprint for a gallery wall using their own images, giving them a visual guidance to install one in their homes.</p>
+            <div id="links">
+
+          <a id ="site" href="https://thegalleryforyou.netlify.app/" target="_blank" rel="noopener noreferrer">Visit Site</a>
+          <a id="code" href ="https://github.com/amandaphail/TheGallery"target="_blank" rel="noopener noreferrer">Code</a>
+            </div>
           </div>
       </div>
     
